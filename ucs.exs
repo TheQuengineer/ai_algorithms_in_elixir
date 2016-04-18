e5074bf63c9642ab0973fcd01d6c4bf2a3376da4
TheQuengineer <qmanthomas@gmail.com>
2016-04-17 18:56:57 -0700

    started initial work on uniform cost search

---
+++ ucs.exs
@@ -0,0 +1,37 @@
+defmodule UniformCostSearch do
+  @moduledoc """
+  The Uniform Cost Search Is a search Technique that is used to determine what
+  action would have the least amount of path cost. It is alot like the Breadth
+  First Search algorithm, only It differs in when the goal test is applied.
+  This module will illustrate an example of how it can be used in the real world.
+  It accompanies the Uniform Cost Search blog post on http://automatingthefuture.com
+  """
+
+  def fetch_projections_from(file) when is_binary(file)  do
+    start_possible_investments_list
+    {_, data} = File.read("data/investmentOptions.txt")
+    data
+    |> String.rstrip(?\n)
+    |> String.split("\n")
+    |> Enum.each(&gather(&1))
+    IO.inspect(Agent.get(PossibleInvestments, &(&1)))
+  end
+
+  defp gather(investment) do
+    data_list = String.split(investment, ",")
+    {company, price} = {List.first(data_list), String.lstrip(List.last(data_list))}
+    build_possible_investments(company, price)
+  end
+
+  defp start_possible_investments_list do
+    Agent.start(fn -> [] end, [name: PossibleInvestments])
+  end
+
+  defp build_possible_investments(company, price) do
+    data = [company_name: company, projected_price: String.to_float(price)]
+    Agent.update(PossibleInvestments, fn(list) -> [data | list] end)
+  end
+end
+
+
+UniformCostSearch.fetch_projections_from("data/investmentOptions.txt")
